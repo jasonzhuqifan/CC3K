7bdcd7f482e3303c27c7d5f73bc6f1ed05b8acdd
Oliver <oliverwang1224@gmail.com>
2017-03-25 21:48:48 -0400

    Chamber Completed

--- Chamber.cc
+++ Chamber.cc
@@ -1,9 +1,40 @@
-//
-//  Chamber.cpp
-//  CC3FinalProject
-//
-//  Created by Raymond Tan on 2017-03-25.
-//
-//
-
 #include "Chamber.h"
+
+Chamber::Chamber() {
+    readMap();
+    scanChamber();
+}
+
+void Chamber::readMap() {
+    ifstream file("cc3kfloor.txt");
+    string line;
+    while(getline(file, line)){
+        vector<char> c_arr;
+        for(int i = 0; i < line.length(); ++i){
+            c_arr.emplace_back(line[i]);
+        }
+        textMap.emplace_back(c_arr);
+    }
+}
+
+void Chamber::scanChamber() {
+    int chamId = 0;
+    for (int row = 0; row < 79; row++) {
+        for (int col = 0; col < 25; col++) {
+            if (textMap[row][col] == '.') {
+                recursiveAdd(row, col, chamId++);
+            }
+        }
+    }
+}
+
+void Chamber::recursiveAdd(int row, int col, int chamId) {
+    chamLst[chamId].emplace_back(pair<int, int>(row,col));
+    textMap[row][col] = ' ';
+    if(textMap[row][col+1] == '.') {
+        recursiveAdd(row, col+1, chamId);
+    }
+    if(textMap[row+1][col] == '.') {
+        recursiveAdd(row, col+1, chamId);
+    }
+}
--- Chamber.h
+++ Chamber.h
@@ -1,16 +1,21 @@
-//
-//  Chamber.hpp
-//  CC3FinalProject
-//
-//  Created by Raymond Tan on 2017-03-25.
-//
-//
-
 #ifndef Chamber_hpp
 #define Chamber_hpp
 
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
 class Chamber{
+    vector<vector<pair<int, int>>> chamLst;
+    vector<vector<char>> textMap;
+    void readMap();
+    void scanChamber();
+    void recursiveAdd(int row, int col, int chamId);
     
+public:
+    Chamber(); //read Text map and scan chamLst
+    vector<vector<pair<int, int>>> getChamberList(); //return vector of Chambers
 };
 
-#endif /* Chamber_hpp */
+#endif
