ed40d26b6c3f1705c8940ee5a5941f582bba8982
jasonzhuqifan <jasonzhu000@hotmail.com>
2017-03-28 00:25:02 -0400

    player

--- Character/Player/Player.h
+++ Character/Player/Player.h
@@ -14,6 +14,7 @@ public:
     double getMaxHP();
     double getGold();
     virtual void attack(std::string dir);
+    void setHealth(double h);
     void move(std::string dir);
     void use(std::string dir);
     GridObjectType getObjType() override;
