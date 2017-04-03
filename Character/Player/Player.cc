bb5624f736e1e0ddd0fc303c10257fa1a9a6d54e
Oliver <oliverwang1224@gmail.com>
2017-04-02 20:19:11 -0400

    Cc

--- Character/Player/Player.cc
+++ Character/Player/Player.cc
@@ -757,105 +757,145 @@ void Player::attackIt(std::shared_ptr<Enemy> e, std::shared_ptr<Player>pc){
 void Player::attackIt(std::shared_ptr<Dwarf> e, std::shared_ptr<Player>pc) {
     double d = e->getDefence();
     double damage = ceil((100/(100+d)) * pc->getAttack());
-    update_message("PC deals ");
-    update_message(to_string(static_cast<int>(damage)));
-    update_message(" damage to Dwarf ");
     e->updateDamage(static_cast<int>(damage));
-    update_message("(");
-    update_message(std::to_string(static_cast<int>(e->getHP())));
-    update_message("/");
-    update_message(std::to_string(static_cast<int>(e->getMaxHP())));
-    update_message(")");
+    if (!e->isDead()) {
+        update_message("PC deals ");
+        update_message(to_string(static_cast<int>(damage)));
+        update_message(" damage to Dwarf ");
+        update_message("(");
+        update_message(std::to_string(static_cast<int>(e->getHP())));
+        update_message("/");
+        update_message(std::to_string(static_cast<int>(e->getMaxHP())));
+        update_message(")");
+    }
+    else {
+        update_message("Dwarf has been slayed by PC. ");
+    }
     check_dead(e);
 }
 
 void Player::attackIt(std::shared_ptr<Dragon> e, std::shared_ptr<Player>pc) {
     double d = e->getDefence();
     double damage = ceil((100/(100+d)) * pc->getAttack());
-    update_message("PC deals ");
-    update_message(to_string(static_cast<int>(damage)));
-    update_message(" damage to Dragon");
-     e->updateDamage(damage);
-    update_message("(");
-    update_message(std::to_string(static_cast<int>(e->getHP())));
-    update_message("/");
-    update_message(std::to_string(static_cast<int>(e->getMaxHP())));
-    update_message(")");
+    e->updateDamage(static_cast<int>(damage));
+    if (!e->isDead()) {
+        update_message("PC deals ");
+        update_message(to_string(static_cast<int>(damage)));
+        update_message(" damage to Dragon ");
+        update_message("(");
+        update_message(std::to_string(static_cast<int>(e->getHP())));
+        update_message("/");
+        update_message(std::to_string(static_cast<int>(e->getMaxHP())));
+        update_message(")");
+    }
+    else {
+        update_message("Dragon has been slayed by PC. ");
+    }
     check_dead(e);
 }
 
 void Player::attackIt(std::shared_ptr<Elf> e, std::shared_ptr<Player>pc) {
     double d = e->getDefence();
     double damage = ceil((100/(100+d)) * pc->getAttack());
-    update_message("PC deals ");
-    update_message(to_string(static_cast<int>(damage)));
-    update_message(" damage to Elf");
-    e->updateDamage(damage);
-    update_message("(");
-    update_message(std::to_string(static_cast<int>(e->getHP())));
-    update_message("/");
-    update_message(std::to_string(static_cast<int>(e->getMaxHP())));
-    update_message(")");
+    e->updateDamage(static_cast<int>(damage));
+    if (!e->isDead()) {
+        update_message("PC deals ");
+        update_message(to_string(static_cast<int>(damage)));
+        update_message(" damage to Elf ");
+        update_message("(");
+        update_message(std::to_string(static_cast<int>(e->getHP())));
+        update_message("/");
+        update_message(std::to_string(static_cast<int>(e->getMaxHP())));
+        update_message(")");
+    }
+    else {
+        update_message("Elf has been slayed by PC. ");
+    }
+
     check_dead(e);
 }
 
 void Player::attackIt(std::shared_ptr<Halfling> e, std::shared_ptr<Player>pc) {
     double d = e->getDefence();
     double damage = ceil((100/(100+d)) * pc->getAttack());
-    update_message("PC deals ");
-    update_message(to_string(static_cast<int>(damage)));
-    update_message(" damage to Hafling");
-     e->updateDamage(damage);
-    update_message("(");
-    update_message(std::to_string(static_cast<int>(e->getHP())));
-    update_message("/");
-    update_message(std::to_string(static_cast<int>(e->getMaxHP())));
-    update_message(")");
+    e->updateDamage(static_cast<int>(damage));
+    if (!e->isDead()) {
+        update_message("PC deals ");
+        update_message(to_string(static_cast<int>(damage)));
+        update_message(" damage to Halfling ");
+        update_message("(");
+        update_message(std::to_string(static_cast<int>(e->getHP())));
+        update_message("/");
+        update_message(std::to_string(static_cast<int>(e->getMaxHP())));
+        update_message(")");
+    }
+    else {
+        update_message("Halfling has been slayed by PC. ");
+    }
+
     check_dead(e);
 }
 
 void Player::attackIt(std::shared_ptr<Human> e, std::shared_ptr<Player>pc) {
     double d = e->getDefence();
     double damage = ceil((100/(100+d)) * pc->getAttack());
-    update_message("PC deals ");
-    update_message(to_string(static_cast<int>(damage)));
-    update_message(" damage to Human");
-    e->updateDamage(damage);
-    update_message("(");
-    update_message(std::to_string(static_cast<int>(e->getHP())));
-    update_message("/");
-    update_message(std::to_string(static_cast<int>(e->getMaxHP())));
-    update_message(")");
+    e->updateDamage(static_cast<int>(damage));
+    if (!e->isDead()) {
+        update_message("PC deals ");
+        update_message(to_string(static_cast<int>(damage)));
+        update_message(" damage to Human ");
+        update_message("(");
+        update_message(std::to_string(static_cast<int>(e->getHP())));
+        update_message("/");
+        update_message(std::to_string(static_cast<int>(e->getMaxHP())));
+        update_message(")");
+    }
+    else {
+        update_message("Human has been slayed by PC. ");
+    }
+
     check_dead(e);
 }
 
 void Player::attackIt(std::shared_ptr<Merchant> e, std::shared_ptr<Player>pc) {
     double d = e->getDefence();
     double damage = ceil((100/(100+d)) * pc->getAttack());
-    update_message("PC deals ");
-    update_message(to_string(static_cast<int>(damage)));
-    update_message(" damage to Merchant");
-     e->updateDamage(damage);
-    update_message("(");
-    update_message(std::to_string(static_cast<int>(e->getHP())));
-    update_message("/");
-    update_message(std::to_string(static_cast<int>(e->getMaxHP())));
-    update_message(")");
+    e->updateDamage(static_cast<int>(damage));
+    if (!e->isDead()) {
+        update_message("PC deals ");
+        update_message(to_string(static_cast<int>(damage)));
+        update_message(" damage to Merchant ");
+        update_message("(");
+        update_message(std::to_string(static_cast<int>(e->getHP())));
+        update_message("/");
+        update_message(std::to_string(static_cast<int>(e->getMaxHP())));
+        update_message(")");
+    }
+    else {
+        update_message("Merchant has been slayed by PC. ");
+    }
+
     check_dead(e);
 }
 
 void Player::attackIt(std::shared_ptr<Orc> e, std::shared_ptr<Player>pc) {
     double d = e->getDefence();
     double damage = ceil((100/(100+d)) * pc->getAttack());
-    update_message("PC deals ");
-    update_message(to_string(static_cast<int>(damage)));
-    update_message(" damage to Orc");
-     e->updateDamage(damage);
-    update_message("(");
-    update_message(std::to_string(static_cast<int>(e->getHP())));
-    update_message("/");
-    update_message(std::to_string(static_cast<int>(e->getMaxHP())));
-    update_message(")");
+    e->updateDamage(static_cast<int>(damage));
+    if (!e->isDead()) {
+        update_message("PC deals ");
+        update_message(to_string(static_cast<int>(damage)));
+        update_message(" damage to Orc ");
+        update_message("(");
+        update_message(std::to_string(static_cast<int>(e->getHP())));
+        update_message("/");
+        update_message(std::to_string(static_cast<int>(e->getMaxHP())));
+        update_message(")");
+    }
+    else {
+        update_message("Orc has been slayed by PC. ");
+    }
+
     check_dead(e);
 }
 
